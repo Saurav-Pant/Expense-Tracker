0bb6af21c36cdafb7aa5f8ce6baf92484b29fbe2
Saurav Pant <pantsaurav005@gmail.com>
2023-10-20 22:02:19 +0530

    some changes done

--- server/index.js
+++ server/index.js
@@ -22,8 +22,6 @@ const corsOpts = {
   origin: "*",
   credentials: true,
   methods: ["GET", "POST", "HEAD", "PUT", "PATCH", "DELETE"],
-  allowedHeaders: ["Content-Type"],
-  exposedHeaders: ["Content-Type"],
 };
 app.use(cors(corsOpts));
 app.options("*", cors());
