ab6dc4e5af7a740bc4b48a3349c19eb1e7e62e3a
Saurav Pant <pantsaurav005@gmail.com>
2023-10-24 00:51:00 +0530

    last debugging of today🥲

--- Client/src/app/base.js
+++ Client/src/app/base.js
@@ -1,5 +1,6 @@
-const BASE_URL_CHECK = "expense-087x.onrender.com";
+const BASE_URL_CHECK = "expense-087x.onrender.com/";
 export const BASE_URL = BASE_URL_CHECK.endsWith("/")
   ? BASE_URL_CHECK.slice(0, -1)
   : BASE_URL_CHECK;
+console.log(BASE_URL_CHECK);
 console.log(BASE_URL);
