87ed3333de70d4eb4dd4eb652d23bbcee544ee7f
Saurav Pant <pantsaurav005@gmail.com>
2023-10-24 00:44:26 +0530

    do it this now

--- Client/src/app/base.js
+++ Client/src/app/base.js
@@ -1,3 +1,5 @@
 const BASE_URL_CHECK = "expense-087x.onrender.com";
-export const BASE_URL = BASE_URL_CHECK.trim().replace(/\/+$/, "");
+export const BASE_URL = BASE_URL_CHECK.endsWith("/")
+  ? BASE_URL_CHECK.slice(0, -1)
+  : BASE_URL_CHECK;
 console.log(BASE_URL);
