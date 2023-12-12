0f254122a09561e5a957c00732a4eca23ab581aa
Saurav Pant <pantsaurav005@gmail.com>
2023-12-12 13:02:56 +0530

    base URl UPDATEd

--- Client/src/app/base.js
+++ Client/src/app/base.js
@@ -1,4 +1,4 @@
-const BASE_URL_CHECK = "expense-087x.onrender.com/";
+const BASE_URL_CHECK = "https://expense-tracker-l62v.vercel.app";
 export const BASE_URL = BASE_URL_CHECK.endsWith("/")
   ? BASE_URL_CHECK.slice(0, -1)
   : BASE_URL_CHECK;
